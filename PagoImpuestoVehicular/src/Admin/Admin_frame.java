105be16475aa72affeb58206b940732c162d579f
misari309 <90524159+misari309@users.noreply.github.com>
2021-10-09 16:15:24 -0500

    ComboBox terminado
    ComboBox listo

--- PagoImpuestoVehicular/src/Admin/Admin_frame.java
+++ PagoImpuestoVehicular/src/Admin/Admin_frame.java
@@ -5,10 +5,13 @@
  */
 package Admin;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import static java.lang.String.valueOf;
 import java.util.ArrayList;
 import static pagoimpuestovehicular.Principal_frame.lista_vehiculos;
 import pagoimpuestovehicular.Vehiculo;
+import static pagoimpuestovehicular.Principal_frame.desc1;
 
 
 /**
@@ -16,7 +19,7 @@ import pagoimpuestovehicular.Vehiculo;
  * @author camil
  */
 public class Admin_frame extends javax.swing.JFrame {
-    public ArrayList<Vehiculo>vehiculos_comboBox = new ArrayList();
+    public ArrayList<Vehiculo>vehiculos_disponibles = new ArrayList();
     /**
      * Creates new form Admin_frame
      */
@@ -26,17 +29,28 @@ public class Admin_frame extends javax.swing.JFrame {
         setResizable(false);
         setLocationRelativeTo(this);
         
-        rellenar1();
-        rellenar2();
-        rellenar3();
-        mostrarTabla();
-        Vehiculo vehiculo_aux = new Vehiculo();vehiculo_aux.setMarca("NISSAN");vehiculo_aux.setLinea("SENTRA");vehiculo_aux.setAgnio("1992");vehiculo_aux.setCodigo(1);vehiculo_aux.setValorI(Float.parseFloat("5000"));vehiculos_comboBox.add(vehiculo_aux);
-        vehiculo_aux.setMarca("NISSAN");vehiculo_aux.setLinea("SENTRA");vehiculo_aux.setAgnio("1995");vehiculo_aux.setCodigo(2);vehiculo_aux.setValorI(Float.parseFloat("7000"));vehiculos_comboBox.add(vehiculo_aux);
-        vehiculo_aux.setMarca("NISSAN");vehiculo_aux.setLinea("SENTRA");vehiculo_aux.setAgnio("2007");vehiculo_aux.setCodigo(3);vehiculo_aux.setValorI(Float.parseFloat("11000"));vehiculos_comboBox.add(vehiculo_aux);
-        vehiculo_aux.setMarca("CHEVROLET");vehiculo_aux.setLinea("SENTRA");vehiculo_aux.setAgnio("1992");vehiculo_aux.setCodigo(1);vehiculo_aux.setValorI(Float.parseFloat("5000"));vehiculos_comboBox.add(vehiculo_aux);
-        vehiculo_aux.setMarca("CHEVROLET");vehiculo_aux.setLinea("SENTRA");vehiculo_aux.setAgnio("1992");vehiculo_aux.setCodigo(1);vehiculo_aux.setValorI(Float.parseFloat("5000"));vehiculos_comboBox.add(vehiculo_aux);
-        vehiculo_aux.setMarca("RENAULT");vehiculo_aux.setLinea("SENTRA");vehiculo_aux.setAgnio("1992");vehiculo_aux.setCodigo(1);vehiculo_aux.setValorI(Float.parseFloat("5000"));vehiculos_comboBox.add(vehiculo_aux);
+        input_marca.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                mostrarLineaVehiculosComboBox((String) input_marca.getSelectedItem());
+            }
+            
+            
+        });
         
+        input_linea.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg1) {
+                mostrarAgnioVehiculosComboBox((String) input_linea.getSelectedItem());      
+            }
+        });
+        
+        input_agnio.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg1) {
+                agregarValor((String) input_agnio.getSelectedItem());      
+            }
+        });
+        
+        mostrarTabla();
+        rellenarAutosDisponibles();
         mostrarMarcasVehiculosComboBox();
     }
 
@@ -58,14 +72,16 @@ public class Admin_frame extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
-        input_valor = new javax.swing.JTextField();
         input_codigo = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
-        input_agnio = new javax.swing.JTextField();
-        input_linea = new javax.swing.JTextField();
         input_marca = new javax.swing.JComboBox<>();
+        input_linea = new javax.swing.JComboBox<>();
+        input_agnio = new javax.swing.JComboBox<>();
+        input_valor = new javax.swing.JLabel();
+        desc1 = new javax.swing.JTextField();
+        jButton2 = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         table_vehiculos.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -122,8 +138,6 @@ public class Admin_frame extends javax.swing.JFrame {
         jLabel6.setText("Año:");
         jPanel2.add(jLabel6);
         jLabel6.setBounds(20, 190, 60, 24);
-        jPanel2.add(input_valor);
-        input_valor.setBounds(100, 230, 140, 24);
         jPanel2.add(input_codigo);
         input_codigo.setBounds(100, 70, 140, 24);
 
@@ -135,10 +149,6 @@ public class Admin_frame extends javax.swing.JFrame {
         });
         jPanel2.add(jButton1);
         jButton1.setBounds(100, 280, 140, 24);
-        jPanel2.add(input_agnio);
-        input_agnio.setBounds(100, 190, 140, 24);
-        jPanel2.add(input_linea);
-        input_linea.setBounds(100, 150, 140, 24);
 
         input_marca.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -147,6 +157,28 @@ public class Admin_frame extends javax.swing.JFrame {
         });
         jPanel2.add(input_marca);
         input_marca.setBounds(100, 110, 140, 26);
+
+        jPanel2.add(input_linea);
+        input_linea.setBounds(100, 150, 140, 26);
+
+        input_agnio.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                input_agnioActionPerformed(evt);
+            }
+        });
+        jPanel2.add(input_agnio);
+        input_agnio.setBounds(100, 190, 140, 26);
+        jPanel2.add(input_valor);
+        input_valor.setBounds(100, 230, 140, 20);
+
+        desc1.setText("jTextField1");
+
+        jButton2.setText("jButton2");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -158,6 +190,12 @@ public class Admin_frame extends javax.swing.JFrame {
                 .addGap(18, 18, 18)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 260, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(34, 34, 34))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(desc1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(28, 28, 28)
+                .addComponent(jButton2)
+                .addGap(185, 185, 185))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -166,7 +204,11 @@ public class Admin_frame extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 326, Short.MAX_VALUE))
-                .addGap(235, 235, 235))
+                .addGap(91, 91, 91)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(desc1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton2))
+                .addGap(120, 120, 120))
         );
 
         pack();
@@ -178,9 +220,9 @@ public class Admin_frame extends javax.swing.JFrame {
             int codigo;
             float valor;
             marca = valueOf(input_marca.getSelectedItem());
-            linea = input_linea.getText();
+            linea = valueOf(input_linea.getSelectedItem());
             codigo = Integer.parseInt(input_codigo.getText());
-            agnio = input_agnio.getText();
+            agnio = valueOf(input_agnio.getSelectedItem());
             valor = Float.parseFloat(input_valor.getText());
 
             if(!"".equals(marca)&&!"".equals(linea)&&!"".equals(codigo)&&!"".equals(agnio)&&!"".equals(valor)){
@@ -196,9 +238,7 @@ public class Admin_frame extends javax.swing.JFrame {
 
         }catch(Exception e){}
         mostrarTabla();
-        input_linea.setText("");
         input_codigo.setText("");
-        input_agnio.setText("");
         input_valor.setText("");
         
     }//GEN-LAST:event_jButton1ActionPerformed
@@ -206,6 +246,14 @@ public class Admin_frame extends javax.swing.JFrame {
     private void input_marcaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_input_marcaActionPerformed
         
     }//GEN-LAST:event_input_marcaActionPerformed
+
+    private void input_agnioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_input_agnioActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_input_agnioActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -268,53 +316,87 @@ public class Admin_frame extends javax.swing.JFrame {
         });
     }
     
-    public void rellenar1(){
-        Vehiculo vehiculo_aux = new Vehiculo();
-        vehiculo_aux.setCodigo(Integer.parseInt("1"));
-        vehiculo_aux.setMarca("NISSAN");
-        vehiculo_aux.setLinea("PATROL");
-        vehiculo_aux.setAgnio("2006");
-        vehiculo_aux.setValorI(Float.parseFloat("349990"));
-        lista_vehiculos.add(vehiculo_aux);
-        
-
+    public void mostrarMarcasVehiculosComboBox(){
+        String marca_p = "mas";
+        for (int i=0; i<vehiculos_disponibles.size();i++) {
+            if (marca_p != vehiculos_disponibles.get(i).getMarca()) {
+                input_marca.addItem(vehiculos_disponibles.get(i).getMarca());
+                marca_p = vehiculos_disponibles.get(i).getMarca();
+            }        
+        }
     }
-    public void rellenar2(){
-        Vehiculo vehiculo_aux = new Vehiculo();
-        vehiculo_aux.setCodigo(Integer.parseInt("2"));
-        vehiculo_aux.setMarca("NISSAN");
-        vehiculo_aux.setLinea("MURANO");
-        vehiculo_aux.setAgnio("2006");
-        vehiculo_aux.setValorI(Float.parseFloat("169990"));
-        lista_vehiculos.add(vehiculo_aux);
-        
-    }
-    public void rellenar3(){
-        Vehiculo vehiculo_aux = new Vehiculo();
-        
-        vehiculo_aux.setCodigo(Integer.parseInt("3"));
-        vehiculo_aux.setMarca("CHEVROLET");
-        vehiculo_aux.setLinea("TRAVERSE");
-        vehiculo_aux.setAgnio("2006");
-        vehiculo_aux.setValorI(Float.parseFloat("184990"));
-        lista_vehiculos.add(vehiculo_aux);
+      
+    public void mostrarLineaVehiculosComboBox(String seleccion){
+        input_linea.removeAllItems();
+        String linea_a = "las"; 
+        for (int i=0; i<vehiculos_disponibles.size(); i++) {
+            if(seleccion.equals(vehiculos_disponibles.get(i).getMarca())){
+                 if (linea_a != vehiculos_disponibles.get(i).getLinea()) {
+                     input_linea.addItem(vehiculos_disponibles.get(i).getLinea());
+                     linea_a = vehiculos_disponibles.get(i).getLinea();
+                 }
+            }  
+        }
     }
     
-    public void mostrarMarcasVehiculosComboBox(){
-        String marcaa = "a";
-        for(int i=0;i<vehiculos_comboBox.size();i++){
-            input_marca.addItem(vehiculos_comboBox.get(i).getMarca());
-                
+    public void mostrarAgnioVehiculosComboBox(String seleccion){
+        input_agnio.removeAllItems();
+        String linea_a = "las"; 
+        for (int i=0; i<vehiculos_disponibles.size(); i++) {
+            if(seleccion.equals(vehiculos_disponibles.get(i).getLinea())){
+                 if (linea_a != vehiculos_disponibles.get(i).getAgnio()) {
+                     input_agnio.addItem(vehiculos_disponibles.get(i).getAgnio());
+                     linea_a = vehiculos_disponibles.get(i).getAgnio();
+                 }
+            }  
         }
+    }
+    
+    public void agregarValor(String seleccion){
+        for (int i=0; i<vehiculos_disponibles.size(); i++) {
+            if (seleccion.equals(vehiculos_disponibles.get(i).getAgnio())) {
+                input_valor.setText(valueOf(vehiculos_disponibles.get(i).getValorI()));
+            } 
+        }
+    }
+    
+    public void rellenarAutosDisponibles(){
+        
+        vehiculos_disponibles.add(new Vehiculo("NISSAN", "SENTRA", "1992", 1, Float.parseFloat("5000")));
+        vehiculos_disponibles.add(new Vehiculo("NISSAN", "SENTRA", "1995", 2, Float.parseFloat("7000")));
+        vehiculos_disponibles.add(new Vehiculo("NISSAN", "SENTRA", "2007", 3, Float.parseFloat("15000")));
+        
+        vehiculos_disponibles.add(new Vehiculo("CHEVROLET", "AVEO", "2002", 4, Float.parseFloat("8000")));
+        vehiculos_disponibles.add(new Vehiculo("CHEVROLET", "AVEO", "2006", 5, Float.parseFloat("14000")));
+        vehiculos_disponibles.add(new Vehiculo("CHEVROLET", "AVEO", "2010", 6, Float.parseFloat("18000")));
+        
+        vehiculos_disponibles.add(new Vehiculo("RENAULT", "LOGAN", "2004", 7, Float.parseFloat("8000")));
+        vehiculos_disponibles.add(new Vehiculo("RENAULT", "LOGAN", "2010", 8, Float.parseFloat("15000")));
+        vehiculos_disponibles.add(new Vehiculo("RENAULT", "LOGAN", "2015", 9, Float.parseFloat("20000")));
+        
+        vehiculos_disponibles.add(new Vehiculo("RENAULT", "ALASKAN", "2007", 7, Float.parseFloat("12000")));
+        vehiculos_disponibles.add(new Vehiculo("RENAULT", "ALASKAN", "2012", 8, Float.parseFloat("16000")));
+        vehiculos_disponibles.add(new Vehiculo("RENAULT", "ALASKAN", "2019", 9, Float.parseFloat("26000")));
+        
+        vehiculos_disponibles.add(new Vehiculo("RENAULT", "CAPTUR", "2008", 7, Float.parseFloat("15000")));
+        vehiculos_disponibles.add(new Vehiculo("RENAULT", "CAPTUR", "2014", 8, Float.parseFloat("19000")));
+        vehiculos_disponibles.add(new Vehiculo("RENAULT", "CAPTUR", "2019", 9, Float.parseFloat("28000")));
+        
+        vehiculos_disponibles.add(new Vehiculo("RENAULT", "ZOE", "2010", 7, Float.parseFloat("11000")));
+        vehiculos_disponibles.add(new Vehiculo("RENAULT", "ZOE", "2016", 8, Float.parseFloat("13000")));
+        vehiculos_disponibles.add(new Vehiculo("RENAULT", "ZOE", "2021", 9, Float.parseFloat("24000")));
+        
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JTextField input_agnio;
+    private javax.swing.JTextField desc1;
+    private javax.swing.JComboBox<String> input_agnio;
     private javax.swing.JTextField input_codigo;
-    private javax.swing.JTextField input_linea;
+    private javax.swing.JComboBox<String> input_linea;
     private javax.swing.JComboBox<String> input_marca;
-    private javax.swing.JTextField input_valor;
+    private javax.swing.JLabel input_valor;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
